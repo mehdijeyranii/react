832f851b58a24a3b8ac312a96fd5969bb8e76a37
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2024-12-31 11:20:31 +0330

    Initial launch of the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-4xl text-rose-500 bg-red-300">
+                Test text in the tailwindCSS
+            </h1>
+        </div>
+    );
+}
+
+export default App;
