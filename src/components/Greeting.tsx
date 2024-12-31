bcf14dafa60354f5b27902d392cbb53838aed19b
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2024-12-31 11:48:06 +0330

    Creating an class component

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,6 +1,10 @@
-// Functional Component
-const Greeting = () => {
-    return <h1>Hello, World!</h1>;
-};
+import { Component, ReactNode } from "react";
+
+// Class Component
+class Greeting extends Component {
+    render(): ReactNode {
+        return <h1>Hello, World!</h1>;
+    }
+}
 
 export default Greeting;
