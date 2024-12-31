eb40b77d320318cefaaa26af0566d527b62d5a27
Mehdi Jeyrani <mehdijeyrani@gmail.com>
2024-12-31 11:56:48 +0330

    How to use components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Greeting from "./components/Greeting";
 
 function App() {
     return (
         <div>
-            <h1 className="text-4xl text-rose-500 bg-red-300">
-                Test text in the tailwindCSS
-            </h1>
+            <Greeting />
         </div>
     );
 }
